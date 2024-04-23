80047dd579cf1743d1be79bc7b0648518184e67f
amitkumar8486 <heyaku101@gmail.com>
2024-04-23 23:28:12 +0530

    feat:introduce spring jpa

--- src/main/java/com/amit/ecommerce/models/Category.java
+++ src/main/java/com/amit/ecommerce/models/Category.java
@@ -1,5 +1,7 @@
 package com.amit.ecommerce.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,9 +9,11 @@ import java.util.List;
 
 @Getter
 @Setter
+@Entity
 public class Category extends BaseModel {
 
     private String name;
     private String description;
+    @OneToMany(mappedBy = "category") /*Consider this relation once only i.e. it is used to avoid creating separate mapping table of product and category.*/
     private List<Product> products;
 }
--- src/main/java/com/amit/ecommerce/models/Product.java
+++ src/main/java/com/amit/ecommerce/models/Product.java
@@ -1,15 +1,21 @@
 package com.amit.ecommerce.models;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
 
     private String name;
     private String description;
-    private Double price;
     private String imageUrl;
+    private Double price;
+    @ManyToOne(cascade = CascadeType.ALL) /*ALL = whatever we do in Product table it will reflect in Category table also.*/
     private Category category;
 }

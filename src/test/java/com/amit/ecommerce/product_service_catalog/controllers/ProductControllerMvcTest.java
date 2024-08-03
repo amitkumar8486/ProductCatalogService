c724c12ff63b998b54ac88b810998eb8dd32f6e4
amitkuamr8486 <heyaku101@gmail.com>
2024-08-03 15:06:45 +0530

    feat: add post unit test

--- src/test/java/com/amit/ecommerce/product_service_catalog/controllers/ProductControllerMvcTest.java
+++ src/test/java/com/amit/ecommerce/product_service_catalog/controllers/ProductControllerMvcTest.java
@@ -1,5 +1,6 @@
 package com.amit.ecommerce.product_service_catalog.controllers;
 
+import com.amit.ecommerce.product_service_catalog.dtos.ProductDto;
 import com.amit.ecommerce.product_service_catalog.models.Product;
 import com.amit.ecommerce.product_service_catalog.services.IProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,7 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -17,8 +18,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ProductController.class)
 class ProductControllerMvcTest {
@@ -37,10 +38,54 @@ class ProductControllerMvcTest {
         product.setName("Iphone15");
         products.add(product);
 
+        Product product2 = new Product();
+        product2.setName("Iphone14");
+        products.add(product2);
+
         when(productService.getAllProducts()).thenReturn(products);
 
         mockMvc.perform(get("/products")).andExpect(status().isOk())
-                .andExpect(content().string(objectMapper.writeValueAsString(products)));
+                .andExpect(content().string(objectMapper.writeValueAsString(products)))
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].name").value("Iphone15"))
+                .andExpect(jsonPath("$[1].name").value("Iphone14"));
+        /*
+        {
+            "name": "Iphone15",  -> $.length == 2, $[0].name == Iphone15
+            "name": "Iphone14
+        }
+        */
+    }
+
+    @Test
+    public void Test_CreateProduct_CreatedSuccessfully() throws Exception {
+        // Arrange
+        Product expectedProduct = new Product();
+        expectedProduct.setName("Macbook Pro");
+        expectedProduct.setId(3L);
+
+        ProductDto productToBeCreated = new ProductDto();
+        productToBeCreated.setName("Macbook Pro");
+        productToBeCreated.setId(3L);
+
+
+        when(productService.createProduct(any(Product.class))).thenReturn(expectedProduct);
+
+        // Act and Assert
+        mockMvc.perform(post("/products").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(productToBeCreated)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(expectedProduct)))
+                .andExpect(jsonPath("$.length()").value(9))
+                .andExpect(jsonPath("$.name").value("Macbook Pro"))
+                .andExpect(jsonPath("$.id").value(3L));
+
+        /*
+        {
+            "name": "Macbook Pro",  -> $.length == 2, $.name == Macbook Pro
+            "id": 2
+        }
+        */
     }
 
 }
